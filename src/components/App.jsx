cb9d153738c10d26fa009dd657f9169ce9530932
Denys <kanedius@gmail.com>
2022-06-11 13:48:18 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,11 @@ import ContactList from './Contacts/Contacts'
 import Filter from './Filter/Filter'
 import s from './Form/Form.module.css';
 import { useSelector } from 'react-redux';
+import { getItems,getFilter } from 'redux/contacts/contact-selectors';
 
  const App =()=>{
-   const contacts = useSelector(state => state.contacts.items)
-   const filter = useSelector(state => state.contacts.filter)
+   const contacts = useSelector(getItems)
+   const filter = useSelector(getFilter)
 
       const normalizedFilter= filter.toLowerCase()
       const visibleContacts = contacts.filter(contact =>contact.name.toLowerCase().includes(normalizedFilter) )
