c8331fa4f672c48d5dec0f817c576c0ead9189ba
Denys <kanedius@gmail.com>
2022-06-20 00:14:36 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,8 @@ import Form from './Form/Form'
 import ContactList from './Contacts/Contacts'
 import Filter from './Filter/Filter'
 import s from './Form/Form.module.css';
-import { useState } from 'react';
-import { useFetchContactsQuery } from 'redux/contacts/contact-slice';
 
  const App =()=>{
-  // const [filter, setFilter] = useState('');
-  // const{data} = useFetchContactsQuery()
-  // const normalizedFilter = filter.toLowerCase()
-  // const visibleContacts = data && data.filter(contact =>contact.name.toLowerCase().includes(normalizedFilter))
-
-  // const changeFilter = e => {
-  // setFilter(e.currentTarget.value)};
-
     return (<div className={s.wrap}>
       <h2>Phonebook</h2>
       <Form/>
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -2,35 +2,41 @@ import PropTypes from 'prop-types';
 import { useFetchContactsQuery } from 'redux/contacts/contact-slice';
 import s from '../Form/Form.module.css';
 import { useDeleteContactMutation } from 'redux/contacts/contact-slice';
-import { useSelector } from 'react-redux'
-import {getFilter } from 'redux/contacts/contact-selectors';;
+import { useSelector } from 'react-redux';
+import { getFilter } from 'redux/contacts/contact-selectors';
 
+function Contacts({ children }) {
+  const filter = useSelector(getFilter);
+  const { data } = useFetchContactsQuery();
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts =
+    data &&
+    data.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  const [deleteContact] = useDeleteContactMutation();
 
-function Contacts({children  }) {
-  const filter = useSelector(getFilter)
-  const{data} = useFetchContactsQuery()
-  const normalizedFilter = filter.toLowerCase()
-  const visibleContacts = data && data.filter(contact =>contact.name.toLowerCase().includes(normalizedFilter))
-  const [deleteContact]= useDeleteContactMutation()
-
-  return data && (
-    <div className={s.wrap}>
-      {children}
-      <ul className={s.list}>
-        {visibleContacts.map(({ id, name, phone  }) => {
-          return (
-            <li className={s.item} key={id}>
-              <span>{name}: {phone}{' '}</span>
-              <button onClick={() => deleteContact(id)}>Delete</button>{' '}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+  return (
+    data && (
+      <div className={s.wrap}>
+        {children}
+        <ul className={s.list}>
+          {visibleContacts.map(({ id, name, phone }) => {
+            return (
+              <li className={s.item} key={id}>
+                <span>
+                  {name}: {phone}{' '}
+                </span>
+                <button onClick={() => deleteContact(id)}>Delete</button>{' '}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    )
   );
 }
 Contacts.propTypes = {
-  
   children: PropTypes.node,
 };
 
